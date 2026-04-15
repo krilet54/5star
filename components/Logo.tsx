66274f6b241b79bd83a114ef3dd7fc208f7c6324
krilet54 <kirpesh54@gmail.com>
2026-04-15 16:47:37 +0530

    chore(brand): use PNG navbar logo and increase its size

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Logo({ className = '' }: { className?: string }) {
   return (
     <Link href="/" className={`flex items-center no-underline ${className}`}>
-      <img src="/starone-logo-dark.jpg" alt="Star One" className="h-8 block" />
+      <img src="/starone-logo-dark.png" alt="Star One" className="h-12 block" style={{ width: 'auto' }} />
     </Link>
   )
 }

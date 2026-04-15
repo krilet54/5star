809272d6e9e1eb6d867a909f3cbda4b8f12d3809
krilet54 <kirpesh54@gmail.com>
2026-04-15 16:36:07 +0530

    fix(brand): use next/image for navbar logo to ensure reliable loading

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Logo({ className = '' }: { className?: string }) {
   return (
     <Link href="/" className={`flex items-center no-underline ${className}`}>
-      <img src="/starone-logo-dark.jpg" alt="Star One" className="h-8 block" />
+      <Image src="/starone-logo-dark.jpg" alt="Star One" width={160} height={40} className="h-8 block" priority />
     </Link>
   )
 }
